fa6b98cd6e950dac6fa09c999b63cc99a8037141
srinath1 <srinath.erp@gmail.com>
2025-01-21 12:48:23 +0100

    checking cache

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
+//cache testing
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
